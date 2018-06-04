f73eb12a654ac4de882f9d087cb84cd79620dabe
SocksAndBoxer <marcguillaume77@gmail.com>
2018-06-04 12:01:43 +0200

    custom input (removed the bar), fixed the font issue

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { createSwitchNavigator, createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { setCustomTextInput, setCustomText } from 'react-native-global-props';
+
 
 import SignUpScreen from './components/Auth/SignUp';
 import LoginScreen from './components/Auth/Login';
@@ -8,6 +10,27 @@ import HomeScreen from './components/Home';
 import SettingsScreen from './components/Settings';
 import LogoutScreen from './components/Logout';
 
+
+const customTextInputProps = {
+  underlineColorAndroid: 'rgba(0,0,0,0)',
+  style: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: 'white',
+  },
+};
+
+const customTextProps = {
+  style: {
+    fontSize: 16,
+    color: 'white'
+  }
+};
+
+setCustomTextInput(customTextInputProps);
+setCustomText(customTextProps);
 
 const styles = StyleSheet.create({
   container: {
