91a908c6bd48c98adc09ee9331636fd006f6cdcf
SocksAndBoxer <marcguillaume77@gmail.com>
2018-06-05 09:25:16 +0200

    resolving conflicts

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { StyleSheet } from 'react-native';
-=======
 import { StyleSheet, View } from 'react-native';
->>>>>>> f73eb12a654ac4de882f9d087cb84cd79620dabe
 import { createSwitchNavigator, createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import { setCustomTextInput, setCustomText } from 'react-native-global-props';
 
@@ -29,13 +25,8 @@ const customTextInputProps = {
 const customTextProps = {
   style: {
     fontSize: 16,
-<<<<<<< HEAD
     color: 'white',
   },
-=======
-    color: 'white'
-  }
->>>>>>> f73eb12a654ac4de882f9d087cb84cd79620dabe
 };
 
 setCustomTextInput(customTextInputProps);
@@ -66,17 +57,10 @@ const AuthStack = createStackNavigator(
 );
 
 const Nav = createDrawerNavigator({
-<<<<<<< HEAD
   Home: HomeScreen,
   Settings: SettingsScreen,
   Logout: LogoutScreen,
 });
-=======
-    Home: HomeScreen,
-    Settings: SettingsScreen,
-    Logout: LogoutScreen,
-})
->>>>>>> f73eb12a654ac4de882f9d087cb84cd79620dabe
 
 export default createSwitchNavigator(
   {
